933a37e6a0e7717a30988165c2d937df4a01436f
J. Simplicio <jessicasimplicio1@gmail.com>
2018-07-31 16:55:52 -0300

    versao 1.0 mobile finalizada

--- js/app.js
+++ js/app.js
@@ -1,7 +1,14 @@
-var map;
+var map1;
+var map2;
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
+  map1 = new google.maps.Map(document.getElementById('map1'), {
+  center: {lat: -23.55052, lng: -46.6333},
+  zoom: 8
+	});
+
+  map2 = new google.maps.Map(document.getElementById('map2'), {
   center: {lat: -23.55052, lng: -46.6333},
   zoom: 8
 	});
 }
+
