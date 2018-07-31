da9d5ffaa70ca6c24feacbdcb29fe438da401e4a
J. Simplicio <jessicasimplicio1@gmail.com>
2018-07-31 14:22:06 -0300

    introducao google maps

--- js/app.js
+++ js/app.js
@@ -1,1 +1,7 @@
-// La funcionalidad de tu proyecto
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+  center: {lat: -23.55052, lng: -46.6333},
+  zoom: 8
+	});
+}
